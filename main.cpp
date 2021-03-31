7eb63829a71de28e80e18e55377f7a9dde698781
yu-hsuan <landychen1111@gmail.com>
2021-03-31 09:02:12 -0700

    add updating main.cpp

--- main.cpp
+++ main.cpp
@@ -15,23 +15,18 @@ AnalogIn Ain(A3);
 uLCD_4DGL uLCD(D1, D0, D2); // serial tx, serial rx, reset pin;
 
 DigitalIn Sample(USER_BUTTON);
-// LocalFileSystem local("local");  // Create the local filesystem under the name "local"
 BufferedSerial pc(USBTX, USBRX);
+
 void LCD();
 void generate_wave(int y);
 void write_file();
-int sample = 100;
-float ADCdata[100];
+float ADCdata[3672];
              
-
+Timer t;
 
 int main()
 {
     printf("START! \n\r");  
-
-    // FILE *fp = fopen("out.txt", "w"); 
-    // fprintf(fp, "Hello World!");
-    // fclose(fp);
     LCD();
 
 }
@@ -45,14 +40,13 @@ void LCD() {
     uLCD.printf("\n   MENU \n\n");
 
     uLCD.color(WHITE);
-    uLCD.printf("  200HZ \n");
-    uLCD.printf("  400HZ \n");
+    uLCD.printf("  10HZ \n");
+    uLCD.printf("  100HZ \n");
     uLCD.printf("  500HZ \n");
-    uLCD.printf("  1000HZ \n");
     int x1 = 20; int y1 = 50;
     uLCD.rectangle(x1, y1, x1+85, y1+15, RED);
 
-    //50 65 80 95 
+    //50 65 80 
     while(1) {
         if(Up.read() > 0.9f) {
             printf("Up botton PRESSED \n\r");
@@ -61,7 +55,7 @@ void LCD() {
                 y1 -= 15;
                 uLCD.rectangle(x1, y1, x1+85, y1+15, RED);                
             }else{
-                y1 = 95;
+                y1 = 80;
                 uLCD.rectangle(x1, y1, x1+85, y1+15, RED);                 
             }
 
@@ -70,7 +64,7 @@ void LCD() {
         if(Down.read() > 0.9f) {
             printf("Down botton PRESSED \n\r");
             uLCD.rectangle(x1, y1, x1+85, y1+15, BLACK);
-            if(y1 < 95) {
+            if(y1 < 80) {
                 y1 += 15;
                 uLCD.rectangle(x1, y1, x1+85, y1+15, RED);
             }else {
@@ -81,8 +75,8 @@ void LCD() {
         }
         //50 65 80 95 
         if(Choose.read() > 0.9f) {
+            printf("Choose botton PRESSED \n\r"); 
             generate_wave(y1);
-            
         }
            
         ThisThread::sleep_for(250ms);
@@ -92,62 +86,54 @@ void LCD() {
 
 void generate_wave(int y) {
     int period = 0;
-    printf("Choose botton PRESSED \n\r"); 
+    int samples = 0;
     switch (y)
     {
     case 50:
-        period = 5000;
+    //10hz
+        period = 100000;
+        samples = 3672;
         break;
+    //100
     case 65:
-        period = 2500;
+        period = 10000;
+        samples = 362;        
         break;
+    //500
     case 80:
         period = 2000;
-        break;
-    case 95:
-        period = 1000;
+        samples = 72;        
         break;
     default:
         break;
     }
-    printf("y = %d, Period(us) = %d\n\r", y, period);
+    float tmp = 0.1f/(period/272);
     printf("Start generate wave form!! \n\r");
+    printf("%d\n\r", samples);
     ThisThread::sleep_for(1s);
+    
     while (Down.read() < 0.9f && Up.read() < 0.9f && Choose.read() < 0.9f && Sample == 1) {
-        //0.1 period
         int idx = 0;
-        for (float i = 0.0f; i <= 0.9f; i += 0.09f) {
+        for (float i = 0.0f; i <= 0.9f; i += (tmp*9)) {
             Aout = i;
             ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            wait_us(0.1*period/10);
         }
-        //0.9 period
-        for (float i = 0.9f; i >= 0.0f; i -= 0.09f) {
+        for (float i = 0.9f; i >= 0.0f; i -= tmp) {
             Aout = i;
             ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            ADCdata[idx++] = Ain;
-            wait_us(0.9*period/10);
         }
     }  
+    
+
     printf("Stop generate wave form! \n\r");
     if(Sample == 0) {
-        // write_file();
-        // printf("Writing over! \n\r");
         printf("Start catching data\n\r");
         ThisThread::sleep_for(200ms);
-        for(int i=0; i<sample; i++) {
+        for(int i=0; i<samples; i++) {
             printf("%f\n\r", ADCdata[i]);
             ThisThread::sleep_for(200ms);
         }
     }
 
 }
-
-
+     
